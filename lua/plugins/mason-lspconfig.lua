b8d236ab3a434049691f25b52e8191ca8384252a
PerplexedTheta <jahdobble@llownd.net>
2024-06-07 15:36:22 +0100

    Milestone: Auto-installing works across the board

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,23 +1,5 @@
-local opts = {
-    ensure_installed = {
-        "efm",
-        "perlnavigator",
-        "clangd",
-        "intelephense",
-        "java_language_server",
-        "jsonls",
-        "yamlls",
-        "tsserver",
-        "bashls",
-        "emmet_ls",
-    },
-
-    automatic_installation = true,
-}
-
 return {
     "williamboman/mason-lspconfig.nvim",
-    opts = opts,
     event = "BufReadPre",
     dependencies = "williamboman/mason.nvim",
 }
---
+++ lua/plugins/mason-tool-installer-nvim.lua
@@ -0,0 +1,42 @@
+local config = function()
+    local masonToolInstaller = require('mason-tool-installer')
+
+    masonToolInstaller.setup({
+        ensure_installed = {
+            "bash-language-server",
+            "clang-format",
+            "clangd",
+            "cpplint",
+            "css-lsp",
+            "efm",
+            "emmet-ls",
+            "eslint-lsp",
+            "fixjson",
+            "hadolint",
+            "html-lsp",
+            "intelephense",
+            "java-language-server",
+            "json-lsp",
+            "lua-language-server",
+            "luacheck",
+            "perlnavigator",
+            "phpcbf",
+            "phpstan",
+            "prettier",
+            "prettierd",
+            "shellcheck",
+            "shfmt",
+            "stylua",
+            "tailwindcss-language-server",
+            "typescript-language-server",
+            "yaml-language-server",
+        },
+        run_on_start = true
+    })
+end
+
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = config,
+    lazy = false,
+}
