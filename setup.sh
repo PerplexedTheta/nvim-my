a0b4bb900d7fa8cd402d4a92111db92cffac09bf
?/θ <jahdobble@llownd.net>
2024-06-11 19:41:13 +0000

    Update file setup.sh

--- setup.sh
+++ setup.sh
@@ -19,8 +19,23 @@ if [[ "$(uname)" == "Darwin" ]]; then
     echo -ne "Fin\n"
 
 elif [[ "$(uname)" == "Linux" ]]; then
-    echo -ne "Not implemented for Linux systems!\n"
-    exit 1
+    echo -ne "WARNING: Assuming you are on a Debian-based distro\n"
+    ## apt check
+    apt update
+    ## nvim & deps
+    apt install build-essential file -y
+    apt install python3 python3-pip -y
+    apt install neovim -y
+    apt install ripgrep -y
+    ## debuggers & lsp
+    apt install luarocks -y
+    apt install default-jre default-jdk -y
+    apt install maven -y
+    apt install nodejs -y
+    apt install golang -y
+
+    ## done
+    echo -ne "Fin\n"
 
 else
     echo -ne "Unable to determine system type\n"
